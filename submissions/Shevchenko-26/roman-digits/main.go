8ac6b264a8c91a3b575d6bcf697babeea1ddce73
dianashevchenko <shevchenko.diana26@gmail.com>
2019-10-24 22:26:42 +0300

    added minor fixes that addressed pull request issues

--- submissions/Shevchenko-26/roman-digits/main.go
+++ submissions/Shevchenko-26/roman-digits/main.go
@@ -9,63 +9,58 @@ import (
 	"strings"
 )
 
-var digitLength = 7
-var digitWidth = 5
+const digitLength, digitWidth = 7, 5
 
 func main() {
-	fmt.Println(os.Args)
 	if len(os.Args) != 2 && len(os.Args) != 4 {
-		log.Println("Need only one input number")
-		log.Println(os.Args)
-		os.Exit(1)
+		log.Fatal("Need only one input number")
 	}
 	symbolFlag := flag.Bool("symbol", false, "set custom symbol for the art")
 	flag.Parse()
+
 	var symbol, number string
 	if *symbolFlag {
-		number = os.Args[3]
-		symbol = os.Args[2]
-	}else{
+		number, symbol = os.Args[3], os.Args[2]
+	} else {
 		number = os.Args[1]
 	}
 	if _, err := strconv.ParseUint(number, 10, 64); err != nil {
-		log.Println("Input argument is integer non-convertible: ", err.Error())
-		os.Exit(1)
+		log.Fatalf("Input argument is integer non-convertible:  %s\n", err)
 	}
 
 	digs := prepareDigits(symbol)
 
-	sb := strings.Builder{}
-	for i := 0; i < 7; i++ {
-		for _, num := range number {
-			sb.WriteString(digs[int(num-48)][i] + " ")
-		}
-		if i != digitLength-1 {
-
-			sb.WriteByte('\n')
-		}
-	}
 	asteriskBorder := strings.Repeat("*", len(number)*(digitWidth+1)-1)
 
-	fmt.Printf("%s\n%s\n%s\n", asteriskBorder, sb.String(), asteriskBorder)
+	numberArr := strings.Split(number, "")
+	var numbers []int
+	for _, num := range numberArr {
+		n, _ := strconv.Atoi(num)
+		numbers = append(numbers, n)
+	}
+
+	fmt.Println(asteriskBorder)
+	for i := 0; i < digitLength; i++ {
+		for _, num := range numbers {
+			fmt.Printf("%s ", digs[num][i])
+		}
+		fmt.Println()
+	}
+	fmt.Println(asteriskBorder)
+
 }
 
-func prepareDigits(symbol string) map[int][]string {
+func prepareDigits(symbol string) [10][]string {
 	artNumbers := strings.Split(digits, "\n\n")
-	numbers := make(map[int][]string)
-	numberHeight := 0
+	numbers := [10][]string{}
 	for i, digit := range artNumbers {
 		if symbol != "" {
 			digit = strings.ReplaceAll(digit, strconv.Itoa(i), symbol)
 		}
-
-		for _, digitString := range strings.Split(digit, "\n") {
-			if digitString == "" {
-				continue
-			}
+		digitRows := strings.Split(strings.Trim(digit, "\n"), "\n")
+		for _, digitString := range digitRows {
 			digitString = fmt.Sprintf("%-5s", digitString)
-			numbers[numberHeight/digitLength] = append(numbers[numberHeight/digitLength], digitString)
-			numberHeight++
+			numbers[i] = append(numbers[i], digitString)
 		}
 	}
 	return numbers
